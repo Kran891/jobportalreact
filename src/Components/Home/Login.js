04d6d0809eb369adc00e9594ff6cc0da27f84027
Anoohya Pureddy <anoohyapureddy@gamil.com>
2023-08-07 17:53:42 +0530

    login model completed

--- src/Components/Home/Login.js
+++ src/Components/Home/Login.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css'
 function Login(props){
-    function LoginUser(){
-
+    function LoginUser(event){ 
+console.log(loginUser)
+event.preventDefault();
     }
+    function handleChange(event){
+      const{name,value}=event.target; console.log(name,value);
+      setLoginUser(previousValue=>{return{
+        ...previousValue,
+        [name]:value
+      }})
+    }
+    const [loginUser,setLoginUser ]=useState({Email:"",Password:""});
     return (
         <form onSubmit={LoginUser} className="login">
           <div className="field">
-            <input type="text" name="Email" placeholder="Email Address" required />
+            <input onChange={handleChange} type="text" name="Email" placeholder="Email Address" required value={loginUser.Email } />
+      
           </div>
           <div className="field">
-            <input type="password" placeholder="Password" required />
+            <input onChange={handleChange} type="password" name= "Password" placeholder="Password" required value={loginUser.Password} />
           </div>
           <div className="field btn">
             <div className="btn-layer"></div>
--- src/Components/Home/UserServer.js
+++ src/Components/Home/UserServer.js
@@ -27,4 +27,33 @@ async function InsertUser(data){
         console.error("Error: ",err);
     }
 }
+async function LoginUser(data){
+
+  try{
+      var headers={
+          
+      }
+    const response=fetch(API+"user/loginuser",
+    {
+      method:'POST',
+      headers:{
+          'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    }
+    ).then(async res=>await res.json())
+    .then(async result=>{
+      const {token,userId}=await result;
+      localStorage.setItem("userId",userId);
+      localStorage.setItem("token",token);
+      return userId;
+
+    }
+      )
+   
+
+  }catch(err){
+      console.error("Error: ",err);
+  }
+}
 export {InsertUser};
