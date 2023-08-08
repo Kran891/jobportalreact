15bb4fc28742961f7337baa60b2502d029efeae8
shubi14 <shubimane1406@gmail.com>
2023-08-09 00:39:02 +0530

    insert company completed

--- src/Components/Company/CompanyServer.js
+++ src/Components/Company/CompanyServer.js
@@ -24,4 +24,31 @@ async function InsertJob(data){
         console.error("Error: ",err);
     }
 }
-export {InsertJob};
+
+
+async function InsertCompanyData(data){
+  try{
+      var headers={
+          
+      }
+    const response=fetch(API+"company/insertcompany",
+    {
+      method:'POST',
+      headers:{
+          'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    }
+    ).then(async res=>await res.json())
+    .then(async result=>{
+      return result;
+
+    }
+      )
+   
+
+  }catch(err){
+      console.error("Error: ",err);
+  }
+}
+export{InsertCompanyData,InsertJob} ;
