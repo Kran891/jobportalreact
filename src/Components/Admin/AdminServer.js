5ed70da022fcdad1c34160656a3d1f5d752c1b99
kran891 <kranthi8919@gmail.com>
2023-08-09 22:29:17 +0530

    adminserver

--- src/Components/Admin/AdminServer.js
+++ src/Components/Admin/AdminServer.js
@@ -1,4 +1,54 @@
 import API from "../API";
 async function unverifiedCompanies(){
-    
+    const response=fetch(API+"admin/getallunverifiedcompanies",
+    {
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    ).then(res=>res.json)
+    .then(result=>{
+        return result;
+    })
+}
+async function verifyaccount(id){
+    const response=fetch(API+"admin/verifyaccount/"+id,
+    {
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    ).then(res=>res.json)
+    .then(result=>{
+        return result;
+    })
+}
+async function getjobpostedtoday(){
+    const response=fetch(API+"admin/getjobpostedtoday/"+id,
+    {
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    ).then(res=>res.json)
+    .then(result=>{
+        return result;
+    })
+
+}
+async function getcompanies(){
+    const response=fetch(API+"admin/getcompanies/"+id,
+    {
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    ).then(res=>res.json)
+    .then(result=>{
+        return result;
+    })
 }
